cce091972ba67d6ed9cc424f50e419ad6ddea3ae
Kulimi <tsaotsaotsao51510@gmail.com>
2021-03-01 18:47:33 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <index msg="Hello Vue 3 + Vite" />
 </template>
 
-<script>
-  import 'bootstrap'
-  import 'bootstrap/dist/css/bootstrap.min.css'
-  export default {
-    name: 'App'
-  }
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+import index from './components/index.vue'
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
 <style>
@@ -21,18 +18,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
-createApp(App).use(router).mount('#app')
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  routes // (缩写) 相当于 routes: routes
+})
+
+
+const app = new Vue({
+  router
+}).$mount('#app')
