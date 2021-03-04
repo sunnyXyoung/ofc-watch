a3795521c4acd34acb773fb6c615c2b6660d01d6
Kulimi <kulimi@caozhengchengde-MacBook-Pro.local>
2021-03-04 14:25:26 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
+
+
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  
+  <div id="app">
+    <mymenu></mymenu>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
+<script>
+  import mymenu from './components/menu.vue';
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+  export default {
+    name: 'Home',
+    components: {
+     mymenu
+    }
+  }
+
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -17,6 +31,18 @@ import HelloWorld from './components/HelloWorld.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/components/menu.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="-menu">
+		<li>1</li>
+		<li>2</li>
+	</div>
+</template>
+
+<style>
+	.-menu {
+		background-color: #123456;
+	}
+
+</style>
