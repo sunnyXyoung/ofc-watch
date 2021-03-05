51c02850b9990d082598129ab8b6114c5a23b11b
Xialai-Kulimi <tsaotsaotsao51510@gmail.com>
2021-03-05 22:37:24 +0800

    add the fucking chart code

--- src/views/Fighter.vue
+++ src/views/Fighter.vue
@@ -19,6 +19,61 @@
 <script>
 import { GChart } from 'vue-google-charts'
 
+import Vue from "vue";
+import VueFusionCharts from "vue-fusioncharts";
+import FusionCharts from "fusioncharts";
+import TimeSeries from "fusioncharts/fusioncharts.timeseries";
+
+// register VueFusionCharts component
+Vue.use(VueFusionCharts, FusionCharts, TimeSeries);
+
+var jsonify = res => res.json();
+var dataFetch = fetch(
+    "https://s3.eu-central-1.amazonaws.com/fusion.store/ft/data/plotting-multiple-series-on-time-axis-data.json"
+).then(jsonify);
+var schemaFetch = fetch(
+    "https://s3.eu-central-1.amazonaws.com/fusion.store/ft/schema/plotting-multiple-series-on-time-axis-schema.json"
+).then(jsonify);
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    width: "100%",
+    height: "400",
+    type: "timeseries",
+    dataFormat: "json",
+    dataSource: {
+      chart: {},
+      caption: {
+        text: "Sales Analysis"
+      },
+      subcaption: {
+        text: "Grocery & Footwear"
+      },
+      series: "Type",
+      yaxis: [
+        {
+          plot: "Sales Value",
+          title: "Sale Value",
+          format: {
+            prefix: "$"
+          }
+        }
+      ]
+    }
+  },
+  mounted: function() {
+    Promise.all([dataFetch, schemaFetch]).then(res => {
+      const data = res[0];
+      const schema = res[1];
+      const fusionTable = new FusionCharts.DataStore().createDataTable(
+          data,
+          schema
+      );
+      this.dataSource.data = fusionTable;
+    });
+  }
+});
 
 export default {
 name: "Fighter",
