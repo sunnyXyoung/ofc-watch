c2fc109d1c72373a66590a3927a4655654fc414d
Kulimi <tsaotsaotsao51510@gmail.com>
2021-03-05 21:01:36 +0800

    add get_match

--- python/get_match.py
+++ python/get_match.py
@@ -5,18 +5,19 @@ import random
 
 
 
-log = open('history_fight.txt', 'a', encoding="utf-8")
+log = open('history_five.txt', 'a', encoding="utf-8")
 time_use = 0
-i = 37904
+
+i = 1
 while True:
-	i += 1
+	
 	time1 = time.time()
 	# time.sleep((0.25 if 0.25-time_use < 0 else 0.25-time_use) + random.random()/4)
 	headers = {
 	    'User-Agent': 'I just wanna know all the special weapon',
 	    'Accept': '*/*',
 	    'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-	    'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50Ijp7ImVtYWlsIjoiaWFsaWF4aW1pbHVrQGdtYWlsLmNvbSIsInVzZXJJZCI6NzQzM30sInYiOjQsImlhdCI6MTYxNDE3NzI2MSwiZXhwIjoxNjI5NzI5MjYxfQ.-kT6RsBRWK9u7cNuggfXNMxi3XIojBE72ctH8BUiMzM',
+	    'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50Ijp7ImVtYWlsIjoiaWFsaWF4aW1pbHVrQGdtYWlsLmNvbSIsInVzZXJJZCI6MjkzOH0sInYiOjUsImlhdCI6MTYxNDk0ODExNCwiZXhwIjoxNjMwNTAwMTE0fQ.LfV71A-l6-UUybC6KzKw_hxDSMBpgVzxOk2g6dwSkHc',
 	    'Origin': 'https://ourfloatingcastle.com',
 	    'Connection': 'keep-alive',
 	    'TE': 'Trailers',
@@ -26,7 +27,7 @@ while True:
 	response = requests.get(f'https://api.ourfloatingcastle.com/api/report/{i}', headers=headers)
 
 	print(response.text)
-	break
+
 
 	if response.text == '{"statusCode":400,"message":"戰報不存在"}':
 		print(f'lastest match: {i}')
@@ -34,6 +35,7 @@ while True:
 		
 	else:
 		log.write(response.text+'\n')
+		i += 1
 	
 		time_use = time.time() - time1
 		print(f'use{time_use}sec')
