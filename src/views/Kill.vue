b9cf45dc408d83aa8da379c38707fdf4c0042659
Xialai-Kulimi <m4st3r0fs3x>
2021-03-16 00:37:55 +0800

    add Kill

--- src/views/Kill.vue
+++ src/views/Kill.vue
@@ -1,10 +1,34 @@
 <template>
-<div></div>
+  <div>
+    <ul>
+      <li v-for="(item, index) in killBoardList" :key="item">
+        {{ index+1 }}: name: {{ item.name }} Kill: {{ item.Kill }}
+      </li>
+    </ul>
+
+  </div>
 </template>
 
 <script>
 export default {
-name: "Kill"
+  name: "Kill",
+  data() {
+    return {
+      killBoardList: [
+        {name: "Kulimi", Kill: 48763},
+        {name: "Kulimi2", Kill: 30678},
+      ]
+    }
+  },
+  mounted: function () {
+    var jsonify = res => res.json();
+    fetch(
+        "http://localhost:3000/Kill.json"
+    ).then(jsonify).then(data => {
+      this.killBoardList = data
+    });
+
+  }
 }
 </script>
 
