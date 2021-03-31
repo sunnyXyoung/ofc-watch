97071f16cd51e59118139635c119e0f31dd6ff76
Xialai-Kulimi <tsaotsaotsao51510@gmail.com>
2021-03-31 19:56:03 +0800

    fixed missing css classes

--- src/App.vue
+++ src/App.vue
@@ -88,7 +88,16 @@ export default {
 
 </script>
 <style lang="scss">
-
+.row-div {
+  display: flex;
+  flex-flow: row;
+}
+.row-div2 {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  flex-flow: row;
+}
 
 a {
   text-decoration: none;
