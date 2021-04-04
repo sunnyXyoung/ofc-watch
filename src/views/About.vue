c6988597dc75efc15fe80f91d4a75e4e78a66684
Xialai-Kulimi <tsaotsaotsao51510@gmail.com>
2021-04-04 15:38:10 +0800

    update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -46,7 +46,7 @@
       </div>
 
       <hr align="center" width="20%" style="margin-bottom: 25px;margin-top: 25px;border-color: rgba(12, 34, 56, 0.5)">
-      資料每十分鐘更新一次，所以不用猛按重新整理。<br>
+      資料每分鐘更新一次，所以請適時搭配重新整理，因為我還沒做及時串流。<br>
       上次更新時間： {{ aboutList[0] }}
     </div>
 
