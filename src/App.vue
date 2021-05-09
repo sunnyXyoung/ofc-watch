522895653d03ea9609fb2f63eaa9ed788a79137c
Xialai-Kulimi <tsaotsaotsao51510@gmail.com>
2021-05-10 00:20:37 +0800

    update 6

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,8 @@ export default {
       options: [
         {text: '第四輪', value: '4'},
         {text: '第五輪', value: '5'},
-        {text: '幕間劇場', value: '5.5'}
+        {text: '幕間劇場', value: '5.5'},
+        {text: '第六輪', value: '6'}
       ],
       sidebar: false
 
