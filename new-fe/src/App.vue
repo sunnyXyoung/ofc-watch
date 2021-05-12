e4791d96a27a39ae6ef0e5fbd7a0bda23ed6b283
Xialai-Kulimi <tsaotsaotsao51510@gmail.com>
2021-05-13 01:05:04 +0800

    update fe

---
+++ new-fe/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <div id="nav">
+
+      <router-link to="/">OFC Watch</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+#app{
+  margin: 5% 10% 0;
+
+  background: hsla(0,0%,100%,.3);
+
+  position: relative;
+  overflow: hidden;
+}
+
+html{
+  background-color: #000000;
+  background-image: url("./assets/big-eye.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: fixed;
+  height: 100%;
+  color: white;
+}
+
+
+#nav{
+  height: 10%;
+  display: flex;
+
+}
+
+#app::before {
+  content: '';
+  background: hsla(0,0%,100%,.3);
+  position: absolute;
+  left: 0;top: 0;right: 0;bottom: 0;
+  height: 100%;
+  width: 100% ;
+  filter: blur(20px);
+  -webkit-filter: blur(20px);
+  z-index: -1;
+  margin: -30px ;
+
+}
+</style>
