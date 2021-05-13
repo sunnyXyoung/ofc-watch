4c74c53cf1902d8e18c1b1220d52e653a74ed7c5
Xialai-Kulimi <m4st3r0fs3x>
2021-05-13 08:10:58 +0800

    update

--- python/auto_updater.py
+++ python/auto_updater.py
@@ -8,5 +8,5 @@ while True:
     os.system('python3 python/ana_data.py 6')
     with open(os.path.join(web_root, "About.json"), 'w', encoding='utf8') as f:
         result = time.localtime(time.time())
-        f.write(f'["{result.tm_year}/{result.tm_mon}/{result.tm_mday} {str(result.tm_hour).zfill(2)}:{str(result.tm_min).zfill(2)}:{str(result.tm_sec).zfill(2)}')
+        f.write(f'["{result.tm_year}/{result.tm_mon}/{result.tm_mday} {str(result.tm_hour).zfill(2)}:{str(result.tm_min).zfill(2)}:{str(result.tm_sec).zfill(2)}]')
     time.sleep(60)
