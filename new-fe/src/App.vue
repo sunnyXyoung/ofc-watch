c7934b613ab42b091fb1c78c61fb94c6e4d547b9
Xialai-Kulimi <tsaotsaotsao51510@gmail.com>
2021-05-18 22:26:58 +0800

    init new-fe

--- new-fe/src/App.vue
+++ new-fe/src/App.vue
@@ -1,137 +1,32 @@
 <template>
-  <v-app id="inspire">
-    <v-app-bar
-        app
-        color="white"
-        flat
-    >
-      <v-container class="py-0 fill-height align-center ">
-        <!--        <v-avatar-->
-        <!--            class="mr-10"-->
-        <!--            color="grey darken-1"-->
-        <!--            size="32"-->
-        <!--        ></v-avatar>-->
-
-
-
-
-            <v-col>
-              <v-row>
-                <v-col
-                    v-for="(link, text) in links"
-                    :key="text"
-                    cols="auto"
-                >
-                  <v-btn
-                      :to="link"
-                      text
-                      class="text-body-1"
-                  >
-                    {{ text }}
-                  </v-btn>
-                </v-col>
-
-                <v-col>
-                  <v-responsive max-width="260">
-                    <v-text-field
-                        dense
-                        flat
-                        hide-details
-                        rounded
-                        solo-inverted
-                    ></v-text-field>
-                  </v-responsive>
-                </v-col>
-                <v-col>
-                  <v-select :items="options"
-                            v-model="round"
-                            label="請選擇輪數"
-                            dense
-                            solo
-                  ></v-select>
-                </v-col>
-
-              </v-row>
-            </v-col>
-
-      </v-container>
-    </v-app-bar>
-
-    <v-main class="grey lighten-3">
-      <v-container>
-        <!--        <v-row>-->
-        <!--          <v-col cols="2">-->
-        <!--            <v-sheet rounded="lg">-->
-        <!--              <v-list color="transparent">-->
-        <!--                <v-list-item-->
-        <!--                    v-for="n in 5"-->
-        <!--                    :key="n"-->
-        <!--                    link-->
-        <!--                >-->
-        <!--                  <v-list-item-content>-->
-        <!--                    <v-list-item-title>-->
-        <!--                      List Item {{ n }}-->
-        <!--                    </v-list-item-title>-->
-        <!--                  </v-list-item-content>-->
-        <!--                </v-list-item>-->
-
-        <!--                <v-divider class="my-2"></v-divider>-->
-
-        <!--                <v-list-item-->
-        <!--                    link-->
-        <!--                    color="grey lighten-4"-->
-        <!--                >-->
-        <!--                  <v-list-item-content>-->
-        <!--                    <v-list-item-title>-->
-        <!--                      Refresh-->
-        <!--                    </v-list-item-title>-->
-        <!--                  </v-list-item-content>-->
-        <!--                </v-list-item>-->
-        <!--              </v-list>-->
-        <!--            </v-sheet>-->
-        <!--          </v-col>-->
-
-        <!--          <v-col>-->
-        <v-sheet
-            min-height="70vh"
-            rounded="lg"
-        >
-          <router-view/>
-        </v-sheet>
-        <!--          </v-col>-->
-        <!--        </v-row>-->
-      </v-container>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-
-export default {
-  name: 'App',
-
-  data: function () {
-    return {
-      round: 6,
-      options: [
-        {text: '第四輪', value: '4'},
-        {text: '第五輪', value: '5'},
-        {text: '幕間劇場', value: '5.5'},
-        {text: '第六輪', value: '6'}
-      ],
-      links: {
-        "OFC Watch": "/",
-        "排行榜": "scoreboard",
-        "個人頁面": "user",
-        "關於": "about",
-
-      }
-    }
-  },
-};
-</script>
 <style>
-router-link {
-  color: black;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- new-fe/src/views/About.vue
+++ new-fe/src/views/About.vue
@@ -1,8 +1,5 @@
 <template>
-<v-container>
-  <v-card-title class="text-h3">
-    關於
-  </v-card-title>
-  <v-card-text class="text-h6">這是一個基於戰報分析做出來的遊戲攻略網站(如果這算攻略的話)因為我無聊很閒做的網站，所以大概看起來很簡陋吧？我從第四輪結束前就開始製作了，一直拖到第七輪都開始了還沒弄好，過程中遭遇了非常多的技術障礙，感謝 @ItisCaleb 的鼎力相助。</v-card-text>
-</v-container>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
 </template>
