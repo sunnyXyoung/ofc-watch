1df0c17751125c33e2a29e5de76e16bfe1350878
Xialai-Kulimi <tsaotsaotsao51510@gmail.com>
2021-05-20 00:10:21 +0800

    update

--- new-fe/src/App.vue
+++ new-fe/src/App.vue
@@ -1,8 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div id="menu">
+        <router-link class="router-link-btn" to="/">OFC Watch</router-link>
+        <router-link class="router-link-btn" to="/leaderboard">排行榜</router-link>
+        <router-link class="router-link-btn" to="/about">關於</router-link>
+        <div>
+          <input type="text"
+                 placeholder="搜尋..."
+          >
+        </div>
+        <div>
+          <select id="round-selector" v-model="round" @change="ChangeRound">
+            <option v-for="option in options" :key="option.value" :value="option.value" >
+              {{ option.text }}
+            </option>
+          </select>
+        </div>
+      </div>
     </div>
     <router-view/>
   </div>
@@ -11,15 +26,23 @@
 export default {
   data() {
     return {
-      login: false
+      login: false,
+      round: 6,
+      options: [
+        {text: '第四輪', value: '4'},
+        {text: '第五輪', value: '5'},
+        {text: '幕間劇場', value: '5.5'},
+        {text: '第六輪', value: '6'}
+      ],
     }
   }
 }
 </script>
 <style>
-html{
+html {
 
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,16 +52,28 @@ html{
 }
 
 #nav {
-  padding-top: 30px;
+  display: flex;
+  flex-flow: row;
+  justify-content: space-around;
+  padding-top: 15px;
+  padding-bottom: 30px;
   width: 100%;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#menu{
+  display: flex;
+  flex-flow: row;
+  justify-content: space-around;
+  width: 50%;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.router-link-btn {
+  font-weight: bold;
+  color: #0c0c0c;
+  text-decoration: none;
+}
+
+.router-link-btn.router-link-exact-active {
+  text-decoration: underline;
 }
 </style>
