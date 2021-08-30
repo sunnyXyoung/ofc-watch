9fec0956fad294143ced396708860ed50539ce7e
Xialai-Kulimi <tsaotsaotsao51510@gmail.com>
2021-08-30 10:29:37 +0800

    update

--- python/ana_data.py
+++ python/ana_data.py
@@ -466,7 +466,7 @@ for i in range(1, len(sorted(match_db)) + 1):
                     # print(weapon['owner'], report['report']['aName'])
                     # print(weapon)
                     global_match_db[report_location][global_match_count[report_location]['count']][
-                        'loot'].append(weapon)
+                        'loot'] = global_match_db[report_location][global_match_count[report_location]['count']]['loot'] + [weapon]
                     print(global_match_db[report_location][global_match_count[report_location]['count']][
                         'loot'])
                     break
