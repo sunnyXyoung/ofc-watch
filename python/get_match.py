e2f13929ad6bfa0583532f99f2d14ef7a2b0104a
Xialai-Kulimi <tsaotsaotsao51510@gmail.com>
2021-09-09 23:20:03 +0800

    update

--- python/get_match.py
+++ python/get_match.py
@@ -5,8 +5,10 @@ from dotenv import load_dotenv
 import sys
 import json
 import logging
+import discord
 load_dotenv()
 
+client = discord.Client()
 
 def get_report(report_id):
     headers = {
@@ -21,57 +23,71 @@ def get_report(report_id):
     get_response = requests.get(f'https://api.ourfloatingcastle.com/api/report/{report_id}', headers=headers)
     return get_response
 
+@client.event
+async def on_ready():
+    print(f'{client.user} online')
+    for g in client.guilds:
+        if g.id == 881543633290035211:
+            for c in g.text_channels:
+                if c.id == 881554191712124989:
+                    lastest_report_c = c
+                    break
+            else:
+                continue
+            break
+    logging.basicConfig(level=logging.NOTSET)
+    try: _round = sys.argv[1]
+    except IndexError: _round = '6'
+    webroot = os.getenv('web-root')
+    initial_wait_time = 10
+    wait_time = initial_wait_time
+    wait_time_rate = 1.5
+    max_wait_time = 600
 
-logging.basicConfig(level=logging.NOTSET)
-try: round = sys.argv[1]
-except IndexError: round = '6'
-webroot = os.getenv('web-root')
-initial_wait_time = 10
-wait_time = initial_wait_time
-wait_time_rate = 1.5
-max_wait_time = 600
+    try: os.mkdir(os.path.join(webroot, 'ofc', _round))
+    except FileExistsError: pass
+    exist_report = {}
+    for i in os.listdir(os.path.join(webroot, 'ofc', _round)):
+        if '.json' in i: exist_report[int(i[:-5])] = i
 
-try: os.mkdir(os.path.join(webroot, 'ofc', round))
-except FileExistsError: pass
-exist_report = {}
-for i in os.listdir(os.path.join(webroot, 'ofc', round)):
-    if '.json' in i: exist_report[int(i[:-5])] = i
+    try:
+        for i in range(1, sorted(exist_report.items())[-1][0]+1):
+            if f'{i}.json' != exist_report.get(i, ''):
+                with open(os.path.join(webroot, 'ofc', _round, f'{i}.json'), 'w', encoding="utf-8") as f:
+                    f.write(json.dumps(json.loads(get_report(i).text), ensure_ascii=False))
+        i = sorted(exist_report.items())[-1][0]
+    except IndexError:
+        i = 1
 
-try:
-    for i in range(1, sorted(exist_report.items())[-1][0]+1):
-        if f'{i}.json' != exist_report.get(i, ''):
-            with open(os.path.join(webroot, 'ofc', round, f'{i}.json'), 'w', encoding="utf-8") as f:
-                f.write(json.dumps(json.loads(get_report(i).text), ensure_ascii=False))
-    i = sorted(exist_report.items())[-1][0]
-except IndexError:
-    i = 1
+    while True:
+        logging.info(i)
+        initial_time = time.time()  # Record initial time.
+        response = get_report(i)
+        logging.info(response.text)
 
-while True:
-    logging.info(i)
-    initial_time = time.time()  # Record initial time.
-    response = get_report(i)
-    logging.info(response.text)
+        if response.status_code == 200:
+            try:
+                line_dict = json.loads(response.text)
+                with open(os.path.join(webroot, 'ofc', _round, f'{i}.json'), 'w', encoding="utf-8") as f:
+                    f.write(json.dumps(line_dict, ensure_ascii=False))
+                await lastest_report_c.send(f"https://ofc-watch.kulimi.tw/history/{_round}/{i}")
+                i += 1
+                wait_time = initial_wait_time
+                continue
+            except Exception as e:
+                logging.error(e)
+                logging.warning(f'Error occur while downloading report. : {i}')
+                logging.warning(response.text)
 
-    if response.status_code == 200:
-        try:
-            line_dict = json.loads(response.text)
-            with open(os.path.join(webroot, 'ofc', round, f'{i}.json'), 'w', encoding="utf-8") as f:
-                f.write(json.dumps(line_dict, ensure_ascii=False))
-            i += 1
-            wait_time = initial_wait_time
-            continue
-        except Exception as e:
-            logging.error(e)
-            logging.warning(f'Error occur while downloading report. : {i}')
-            logging.warning(response.text)
+        elif response.status_code == 400:
+            print(f'Last report: {i}')
 
-    elif response.status_code == 400:
-        print(f'Last report: {i}')
-    
-    time_use = time.time() - initial_time
-    print(f'use {time_use}sec')
+        time_use = time.time() - initial_time
+        print(f'use {time_use}sec')
 
-    time.sleep(wait_time)
-    wait_time = wait_time * wait_time_rate
-    if wait_time > max_wait_time:
-        wait_time = max_wait_time
+        time.sleep(wait_time)
+        wait_time = wait_time * wait_time_rate
+        if wait_time > max_wait_time:
+            wait_time = max_wait_time
+
+client.run("NjkxNjA5NjQzMzMxNDg1NzY3.XnidrA.HPmCdPi2rOsji6uugRs5N9KiCO8")
