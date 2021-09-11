97836fd7a2a29b7e2fe2ac76cf0a090a12ec8591
Xialai-Kulimi <m4st3r0fs3x>
2021-09-12 02:16:25 +0800

    update

--- python/get_leaderboard.py
+++ python/get_leaderboard.py
@@ -15,12 +15,48 @@ board_to_channel = {
     'fight_exp': '戰熟追蹤',
     'mine_exp': '礦熟追蹤',
     'forge_exp': '鍛熟追蹤',
+    "role2": '副職追蹤',
 }
+
+channel_to_board = {
+    '金錢追蹤': 'money',
+    '戰熟追蹤': 'fight_exp',
+    '礦熟追蹤': 'mine_exp',
+    '鍛熟追蹤': 'forge_exp',
+    '副職追蹤': 'role2',
+}
+
+nl_dict = {
+    'money': '元',
+    'fight_exp': '點戰熟',
+    'mine_exp': '點礦熟',
+    'forge_exp': '點鍛熟'
+}
+
+nl_dict2 = {
+    'money': '金錢',
+    'fight_exp': '戰熟',
+    'mine_exp': '礦熟',
+    'forge_exp': '鍛熟',
+
+}
+
 logging.basicConfig(level=logging.NOTSET)
+
+
+def sec_to_text(sec):
+    if sec>60:
+        if int(int(sec/60)/60) == 0:
+            return f' {int(sec-int(int(sec/60)/60))/60} 分鐘'
+        else:
+            return f' {int(int(sec/60)/60)} 小時 {int(int(sec-int(int(sec/60)/60)*3600)/60)} 分鐘'
+    else:
+        return f' {sec} 秒'
+
 
 async def update_leaderboard():
     
-    leaderboards = ['money', 'fight_exp', 'forge_exp', 'mine_exp']
+    leaderboards = ['money', 'fight_exp', 'forge_exp', 'mine_exp', 'kill']
     headers = {
         'User-Agent': 'I just wanna know all the secret data',
         'Accept': '*/*',
@@ -35,10 +71,9 @@ async def update_leaderboard():
         while True:
             response = requests.get(f'https://api.ourfloatingcastle.com/api/users?order={i}', headers=headers)
             if response.status_code != 200:
-                logging.error(faction_dict, faction_dict.text, faction_dict.status_code)
                 error_count += 1
                 if error_count > 10:
-                    logging.error('over 10 errors')
+                    logging.error(response, 'over 10 errors')
                     exit()
             else:
                 break
@@ -55,18 +90,66 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} online')
     for g in client.guilds:
-        if g.id == 881543633290035211:
+        if g.id == 885550088410255392:
             break
 
 
-    leaderboard = {}
+    prev_leaderboard = {}
+
     while True:
         new_leaderboard = await update_leaderboard()
+        new_leaderboard_dict = {}
+        for topic in new_leaderboard:
+            topic_leaderboard = new_leaderboard[topic]
+            for p in topic_leaderboard:
+                p['board'] = new_leaderboard_dict.get(p.id, {}).get('board', []) + [topic]
+                new_leaderboard_dict[p.id] = p.copy()
+
+        text = {}
+        back_leaderboard_dict = new_leaderboard_dict.copy()
+        for _p in new_leaderboard_dict:
+            p = new_leaderboard_dict[_p]
+            prev_p = prev_leaderboard.get(p.id, {})
+            if prev_p.get('board'):
+                for stat in nl_dict:
+                    if p[stat] != prev_p[stat]:
+                        p[stat+'_last_update'] = time.time()
+                        if p[stat] > prev_p[stat]: 
+                            text[stat] = f'【{p.get("factionName")}】{p.get("nickname")}[{p.get("role")}]({p.get("role2")}) 在{sec_to_text(p.get(stat+"_last_update", 0) - prev_p.get(stat+"_last_update", 0))}內增加了 **{p[stat] - prev_p[stat]}** {nl_dict[stat]} ({prev_p[stat]} --> {p[stat]})'
+                        else:
+                            assert p[stat] < prev_p[stat]
+                            text[stat] = f'【{p.get("factionName")}】{p.get("nickname")}[{p.get("role")}]({p.get("role2")}) 在{sec_to_text(p.get(stat+"_last_update", 0) - prev_p.get(stat+"_last_update", 0))}內減少了 **{prev_p[stat] - p[stat]}** {nl_dict[stat]} ({prev_p[stat]} --> {p[stat]})'
+                    
+                if p.get('role2') != prev_p.get('role2'):
+                    p['roles_last_update'] = time.time()
+                    text['role2'] = text[stat] = f'【{p.get("factionName")}】{p.get("nickname")}[{p.get("role")}]({p.get("role2")}) 在{sec_to_text(p.get("role2_last_update", 0) - prev_p.get("role2_last_update", 0))}內更換了副職業 ({prev_p['role2']} --> {p['role2']})'
+                prev_leaderboard.pop(p.id)
+            else:
+                for stat in p['board']:
+                    if stat == 'kill':
+                        continue
+                    text[stat] = f'【{p.get("factionName")}】{p.get("nickname")}[{p.get("role")}]({p.get("role2")}) 登上了{"、".join(p['board'])}榜 ({p[stat]}{nl_dict[stat]})'
+
+            back_leaderboard_dict[p.id] = p.copy()
+            for c in g.text_channels:
+                if c.name in channel_to_board:
+                    await c.send(text[channel_to_board[c.name]])
+        for _p in prev_leaderboard:
+            prev_p = prev_leaderboard[_p]
+            for stat in prev_p['board']:
+                if stat == 'kill':
+                    continue
+                text[stat] = f'【{prev_p.get("factionName")}】{prev_p.get("nickname")}[{prev_p.get("role")}]({prev_p.get("role2")}) 從{"、".join(prev_p['board'])}榜上消失了 (原本有{p[stat]}{nl_dict[stat]})'
+            prev_p['board'] = []
+            back_leaderboard_dict[prev_p.id] = prev_p.copy()
+
+        prev_leaderboard = back_leaderboard_dict.copy()
+
+
+
 
         
-        for c in g.text_channels:
-            if c.name == faction:
-                await c.send(text)
+            
 
         await asyncio.sleep(10)
 
