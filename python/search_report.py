675a2e13248031479f82be4b4026b0dcb6b7e3e5
Xialai-Kulimi <tsaotsaotsao51510@gmail.com>
2021-09-13 00:19:44 +0800

    Update search_report.py

--- python/search_report.py
+++ python/search_report.py
@@ -1,4 +1,3 @@
-from discord import Client
 from discord_components import *
 import discord_components
 import discord
@@ -7,13 +6,14 @@ import time
 import random
 import datetime
 import asyncio
-
+from dotenv import load_dotenv
+import sys
+load_dotenv()
 
 
 intents = discord.Intents.default()
 intents.members = True
-
-client = Client(intents=intents)
+client = discord.Client(intents=intents)
 
 
 @client.event
@@ -24,20 +24,32 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    
-    
     if message.content.strip() == 'k!search':
         await message.reply('''```
-k!search [-p player_name,player_name2] [-l location] [-w weapon_type,weapon_type2,...] ["search_text"]
+k!search [-r round] [-p player_name,player_name2] [-l location] [-w weapon_type,weapon_type2,...] ["search_text"]
+-r 指定輪數
 -p 指定玩家名稱，若玩家名稱包含在內就會顯示
 -l 指定位置
 -w 指定武器類別
 "search_text" 指定所有包含該段文字的戰報
+
+除-r為必填以外，其餘欄位最少要指定一個。
 ```''')
 
 
     elif message.startswith('k!search '):        
-        seach_text = message.content.strip()[len('k!search '):].lower()
+        search_text = message.content.strip()[len('k!search '):].lower()
+        search_text = search_text.split(' ')
+        search_params = []
+        for i in search_text:
+            if i:
+                search_params.append(i)
+
+        if '-r' in search_params and search_params.index('-r') != len(search_params)-1:
+            
+
+        else:
+            await message.reply('未指定輪數 請使用 `k!search` 查看更多資訊')
         
 
-client.run("Njk3NjYyMDA4Mzc1Mzc3OTY1.Xo6iYA.noG-FcNH3QTxcKFduexwoUWfO6w")
+client.run(os.getenv('siesta-token'))
