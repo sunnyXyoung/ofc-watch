36a3a3fd1454df99ba18d739f8953825c7761d8e
Xialai-Kulimi <tsaotsaotsao51510@gmail.com>
2021-09-14 00:56:09 +0800

    update

--- python/search_report.py
+++ python/search_report.py
@@ -9,6 +9,7 @@ import asyncio
 from dotenv import load_dotenv
 import sys
 import os
+import search_filter
 load_dotenv()
 
 
@@ -54,25 +55,28 @@ async def on_message(message):
 -l 指定位置
 -w 指定武器類別
 -wn 指定武器名稱
+-f 指定陣營，須完全正確
 -s 指定所有包含該段文字的戰報
 
 -Wp 指定贏家玩家名稱，若玩家名稱包含在內就會顯示
 -Wr 指定贏家職業
 -Wr2 指定贏家副職業
 -Ww 指定贏家武器類別
+-Wf 指定贏家陣營，須完全正確
 -Wwn 指定贏家武器名稱
 
 -Lp 指定輸家玩家名稱，若玩家名稱包含在內就會顯示
 -Lr 指定輸家職業
 -Lr2 指定輸家副職業
 -Lw 指定輸家武器類別
+-Lf 指定輸家陣營，須完全正確
 -Lwn 指定輸家武器名稱
 
-除-r為必填以外，其餘欄位最少要指定一個。
-範例：`k!search -s "第 74 層被摧毀了"`
-範例：`k!search -p Kulimi 層被摧毀了`
-範例：`k!search -p "Kulimi Beta" 層被摧毀了`
-範例：`k!search -r 礦工,戰鬥員`
+除-R為必填以外，其餘欄位最少要指定一個。
+範例：`k!search -R 9 -s "第 74 層被摧毀了"`
+範例：`k!search -R 6 -p Kulimi -s 層被摧毀了`
+範例：`k!search -R 5 -p "Kulimi Beta" -s 層被摧毀了`
+範例：`k!search -R 5.5 -r 礦工,戰鬥員`
 ''')
     elif message.content.startswith('k!search '):
         if 885399145005846538 not in [r.id for r in message.author.roles]:
@@ -86,8 +90,8 @@ async def on_message(message):
         temp_param = ''
         start_quote = False
         for i in search_text:
-            if i == ' ' and not start_quote:
-                if temp_param != '' and temp_param != '\n':
+            if (i == ' ' or i == '\n') and not start_quote:
+                if temp_param != '':
                     search_params.append(temp_param)
                     temp_param = ''
             elif i == '"':
@@ -167,18 +171,155 @@ async def on_message(message):
                         
                         ans_list = []
                         for report_f in os.listdir(os.path.join(webroot, 'ofc', _round)):
+                            break
                             with open(os.path.join(webroot, 'ofc', _round, report_f), 'r', encoding='utf8') as json_file:
                                 report = json.loads(json_file.read())
-                                report = report['report']
-
+                                # report = report['report']
                                 pass_check = True
                                 for test in params:
-                                    if test_fail:
+                                    if getattr(search_filter.replace('-', ''), test)(report):
                                         pass_check = False
                                         break
 
                                 if pass_check:
                                     ans_list.append(report_f.replace('.json', ''))
+                        if len(ans_list) == 0:
+                            ans_embed = discord.Embed(
+                                title = "無符合的結果", 
+                                colour = discord.Colour.dark_magenta(),
+                                description = 'k!search 戰報查詢系統',
+                                timestamp = datetime.datetime.utcfromtimestamp(time.time()),
+                            )
+                            ans_embed.set_author(name=str(message.author), icon_url=message.author.avatar_url)
+                            await search_m.reply(embed=ans_embed, components=[])
+                        else:
+                            ans_embed = discord.Embed(title="生成結果中，請稍後")
+                            ans_m = await search_m.reply(embed=ans_embed)
+                            while True:
+                                ans_embed = discord.Embed(
+                                    title = f"搜尋結果：共筆 {len(ans_list)} 戰報符合條件",
+                                    colour = discord.Colour.dark_blue(),
+                                    description = 'k!search 戰報查詢系統',
+                                    timestamp = datetime.datetime.utcfromtimestamp(time.time()),
+                                )
+                                ans_embed.set_author(name=str(message.author), icon_url=message.author.avatar_url)
+                                ans_embed.set_footer(text=f"第 {_round} 輪")
+                                await ans_m.edit(embed=ans_embed, components=[
+                                        [[
+                                            Button(style=ButtonStyle.gray, label="預覽戰報"),
+                                            Button(style=ButtonStyle.gray, label="查看列表"),
+                                            Button(style=ButtonStyle.red, label="退出")
+                                        ]]
+                                    ])
+                                while True:
+                                    try:
+                                        res = await client.wait_for("button_click", timeout=60.0)
+                                        if res.message.id == ans_m.id:
+                                            await res.respond(type=6)
+                                            break
+                                    except Exception as e:
+                                        print(e)
+                                        if time.time() - wait_time > 60:
+                                            refuse = True
+                                            ans_embed = discord.Embed(
+                                                title = f"搜尋結果：共筆 {len(ans_list)} 戰報符合條件",
+                                                colour = discord.Colour.red(),
+                                                description = 'k!search 戰報查詢系統',
+                                                timestamp = datetime.datetime.utcfromtimestamp(time.time()),
+                                            )
+                                            ans_embed.set_footer(text=f"第 {_round} 輪")
+                                            ans_embed.set_author(name=str(message.author), icon_url=message.author.avatar_url)
+                                            ans_embed.insert_field_at(index=0, name="閒置過久，已退出檢視")
+                                            await ans_m.edit(embed=ans_embed, components=[])
+                                            return
+    
+                                if res.component.label == "退出":
+                                    ans_embed = discord.Embed(
+                                        title = f"搜尋結果：共筆 {len(ans_list)} 戰報符合條件",
+                                        colour = discord.Colour.red(),
+                                        description = 'k!search 戰報查詢系統',
+                                        timestamp = datetime.datetime.utcfromtimestamp(time.time()),
+                                    )
+                                    ans_embed.set_footer(text=f"第 {_round} 輪")
+                                    ans_embed.set_author(name=str(message.author), icon_url=message.author.avatar_url)
+                                    ans_embed.insert_field_at(index=0, name="已退出檢視")
+                                    await ans_m.edit(embed=ans_embed, components=[])
+                                    return
+                                elif res.component.label == "查看列表":
+                                    text = '\n'.join([f"https://ofc-watch.kulimi.tw/history/{_round}/{i}" for i in ans_list])
+                                    if text > 1024:
+                                        ans_embed = discord.Embed(
+                                            title = f"搜尋結果：共 {len(ans_list)} 筆戰報符合條件",
+                                            colour = discord.Colour.red(),
+                                            description = 'k!search 戰報查詢系統',
+                                            timestamp = datetime.datetime.utcfromtimestamp(time.time()),
+                                        )
+                                        ans_embed.set_footer(text=f"第 {_round} 輪")
+                                        ans_embed.set_author(name=str(message.author), icon_url=message.author.avatar_url)
+                                        ans_embed.remove_field(0)
+                                        ans_embed.insert_field_at(index=0, name="戰報過多，無法查看列表")
+                                        await ans_m.edit(embed=ans_embed)
+                                    else:
+                                        ans_embed.remove_field(0)
+                                        ans_embed.insert_field_at(index=0, name=f'結果列表', value=text, inline=False)
+                                        await ans_m.edit(embed=ans_embed)
+                                elif res.component.label == "預覽戰報":
+                                    index = 0
+                                    ans_len = len(ans_list)
+                                    while True:
+                                        with open(os.path.join(webroot, 'ofc', _round, ans_list+'.json'), 'r', encoding='utf8') as json_file:
+                                            report = json.loads(json_file.read())
+                                            ans_embed = search_filter.report_to_embed(report, _round, f"搜尋結果：共 {len(ans_list)} 筆戰報符合條件")
+                                            await ans_m.edit(embed=ans_embed, components=[
+                                                    [[
+                                                        Button(style=ButtonStyle.gray, label="上一份戰報"),
+                                                        Button(style=ButtonStyle.gray, label="下一份戰報"),
+                                                        Button(style=ButtonStyle.blue, label="回到結果"),
+                                                        Button(style=ButtonStyle.red, label="退出")
+                                                    ]]
+                                                ])
+                                        while True:
+                                            try:
+                                                res = await client.wait_for("button_click", timeout=60.0)
+                                                if res.message.id == ans_m.id:
+                                                    await res.respond(type=6)
+                                                    break
+                                            except Exception as e:
+                                                print(e)
+                                                if time.time() - wait_time > 60:
+                                                    refuse = True
+                                                    ans_embed = discord.Embed(
+                                                        title = f"搜尋結果：共筆 {len(ans_list)} 戰報符合條件",
+                                                        colour = discord.Colour.red(),
+                                                        description = 'k!search 戰報查詢系統',
+                                                        timestamp = datetime.datetime.utcfromtimestamp(time.time()),
+                                                    )
+                                                    ans_embed.set_footer(text=f"第 {_round} 輪")
+                                                    ans_embed.set_author(name=str(message.author), icon_url=message.author.avatar_url)
+                                                    ans_embed.insert_field_at(index=0, name="閒置過久，已退出檢視")
+                                                    await ans_m.edit(embed=ans_embed, components=[])
+                                                    return
+                                        if res.component.label == '上一份戰報':
+                                            index -= 1
+                                        elif res.component.label == '下一份戰報':
+                                            index += 1
+                                        elif res.component.label == '回到結果':
+                                            break
+                                        elif res.component.label == '退出':
+                                            ans_embed = discord.Embed(
+                                                title = f"搜尋結果：共筆 {len(ans_list)} 戰報符合條件",
+                                                colour = discord.Colour.red(),
+                                                description = 'k!search 戰報查詢系統',
+                                                timestamp = datetime.datetime.utcfromtimestamp(time.time()),
+                                            )
+                                            ans_embed.set_footer(text=f"第 {_round} 輪")
+                                            ans_embed.set_author(name=str(message.author), icon_url=message.author.avatar_url)
+                                            ans_embed.insert_field_at(index=0, name="已退出檢視")
+                                            await ans_m.edit(embed=ans_embed, components=[])
+                                            return
+
+
+
 
                     else:
                         search_embed = discord.Embed(
