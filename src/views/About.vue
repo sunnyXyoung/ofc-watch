bbd1480ffcf9790712c1bbea18751aa94174ee40
Kulimi <tsaotsaotsao51510@gmail.com>
2022-02-24 15:04:21 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container
+      fluid
+      row
+      class="align-center justify-center"
+  >
+    <v-card
+        class="ma-5"
+        height="200"
+        width="200"
+        img="https://kulimi.tw/bg.png"
+
+    ></v-card>
+    <v-card class="ma-5"
+
+    >
+      <v-card-title>關於作者</v-card-title>
+      <v-card-subtitle>自我介紹</v-card-subtitle>
+      <v-card-text>暱稱為Kulimi，自詡遊戲中的賺錢大師，興趣為提醒其他玩家尿尿，雖然沒有很閒但還是抽空做出第二版。</v-card-text>
+    </v-card>
+    <v-card class="ma-5"
+
+    >
+      <v-card-title>關於網站</v-card-title>
+      <v-card-subtitle>網站介紹</v-card-subtitle>
+      <v-card-text>這是一個為<a href="https://ourfloatingcastle.com/">我們的浮游城</a>做的基於戰報分析做出來的遊戲攻略網站(如果這算攻略的話)
+        因為我無聊很閒做的網站，所以大概看起來很簡陋吧？
+        我從第四輪結束前就開始製作了，一直拖到第七輪都開始了還沒弄好，
+        過程中遭遇了非常多的技術障礙，感謝 <a href="https://github.com/ItisCaleb">@ItisCaleb</a> 的鼎力相助。
+        GitHub Repo: <a href="https://github.com/Xialai-Kulimi/ofc-watch">https://github.com/Xialai-Kulimi/ofc-watch</a>
+      這個網站所有的資訊都是從戰報抓的，所以大概算是公開的資訊。
+      而且對茅場的伺服器造成的負擔應該很小（吧?
+      所以不用擔心影響效能的問題。 <br> <br>
+        @sunny_young 什麼時候更新OFC
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
